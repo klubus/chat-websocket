e5160da3b1d633a502a6e9923383a4bda50f3f4b
Krystian Kluba <klubakrystian@gmail.com>
2026-01-28 17:21:28 +0100

    client

---
+++ client/app.js
@@ -0,0 +1,52 @@
+let userName = null;
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+console.log('login works', userName);
+
+function login(e) {
+  e.preventDefault();
+  if (!userNameInput.value.trim()) {
+    alert('User name field is empty');
+  } else {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+    addMessageForm.classList.add('show');
+  }
+}
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
+function sendMessage(e) {
+  e.preventDefault();
+  if (!messageContentInput.value.trim()) {
+    alert('Message field is empty');
+  } else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  }
+}
+
+loginForm.addEventListener('submit', (e) => {
+  login(e);
+});
+
+addMessageForm.addEventListener('submit', (e) => {
+  sendMessage(e);
+});
